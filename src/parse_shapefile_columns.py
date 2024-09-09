9ad1d4ad7d8d56f72a2038b6a3acdf3704701d64
bertrancodes <bertran.mb97@gmail.com>
2024-09-09 16:26:05 +0200

    Add docstrings, code refactoring

--- src/parse_shapefile_columns.py
+++ src/parse_shapefile_columns.py
@@ -1,19 +1,32 @@
+from pathlib import Path
+
 import geopandas as gpd
 
 from definitions import DATA_PATH
 
-towns = gpd.read_file(DATA_PATH / "shapefiles" / "towns.shp")
-towns = towns.rename(
-    columns={
-        "NAMEUNIT": "town_name",
-        "NAMEUNIT_2": "province",
-        "NAMEUNIT_3": "region",
-    }
-)
 
-# Select the regions' proper name
-towns["region"] = towns["region"].apply(lambda x: x.split("/")[-1])
-towns.loc[towns["region"] == "Comunitat Valenciana", "region"] = "País Valencià"
-towns["province"] = towns["province"].apply(lambda x: x.split("/")[0])
+def main(file: str | Path) -> gpd.GeoDataFrame:
+    """
+    Parse the geometry file columns' names
+    """
+    towns = gpd.read_file(file)
+    towns = towns.rename(
+        columns={
+            "NAMEUNIT": "town_name",
+            "NAMEUNIT_2": "province",
+            "NAMEUNIT_3": "region",
+        }
+    )
 
-towns.to_file(DATA_PATH / "shapefiles" / "towns_v2.shp")
+    # Select the regions' proper name
+    towns["region"] = towns["region"].apply(lambda x: x.split("/")[-1])
+    towns.loc[towns["region"] == "Comunitat Valenciana", "region"] = "País Valencià"
+    towns["province"] = towns["province"].apply(lambda x: x.split("/")[0])
+
+    return towns
+
+
+if __name__ == "__main__":
+    file = DATA_PATH / "shapefiles" / "towns.shp"
+    towns = main(file=file)
+    towns.to_parquet(DATA_PATH / "shapefiles" / "towns_v2.parquet")
